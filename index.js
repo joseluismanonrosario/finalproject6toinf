03432cd2a69ae863b94a0a0a43a6674ee2c4aad8
Jose Luis Mañon <joseluismanonrosario@gmail.com>
2021-05-27 09:59:44 -0400

    Agregadas las rutas a mis paginas

--- index.js
+++ index.js
@@ -1,13 +1,53 @@
 const express = require('express');
+const hbs = require('express-handlebars');
+const path = require('path');
+
+
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
+// Settings
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
+
+//Handlebars Config
+app.engine(
+    '.hbs', 
+    hbs({
+        defaultLayout: 'main',
+        layoutDir: path.join(app.get('views'), 'layouts'),
+        partialsDir: path.join(app.get('views'), 'partials'),
+        extname: '.hbs'
+    })
+);
+
 
 app.get('/', (req, res)=>{
-    res.send('Ruta de inicio de nuestro proyecto');
+    res.render('index');
+});
+
+app.get('/works', (req, res)=>{
+    res.render('works');
+});
+
+app.get('/services', (req, res)=>{
+   res.render('services');
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about');
+});
+
+app.get('/contact', (req, res)=>{
+    res.render('contact');
+});
+
+app.use((req, res)=>{
+    res.render('404');
 });
 
 app.listen(PORT, ()=>{
-    console.log (`Server on http://localhost:${PORT}`);
+    console.log (`Server at http://localhost:${PORT}`);
 });
 
